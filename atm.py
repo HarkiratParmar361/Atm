737cffc503f39d0b3890ae01b47c5785ab2870d3
HarkiratParmar361 <71603872+HarkiratParmar361@users.noreply.github.com>
2021-09-07 11:43:26 -0400

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,18 @@
+class Atm:
+    def __init__(self,account,cardnumber,pin):
+        self.cardnumber = cardnumber
+        self.pin = pin
+        self.Account = account
+
+        def check_balance(self):
+         print("Total")
+        print("s1 = 1000")
+        print("s2 = 270")
+        print("s3 = 380")
+
+        def main():
+        print("Bank$")
+    account = input("Please put your acount pin")
+    cardNumber = input("Please enter card pin")
+
+main()
